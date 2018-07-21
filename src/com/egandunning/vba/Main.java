dbf910a7253128519380f3e2b485f2b3d2d4e6cb
Egan <dunninea@plu.edu>
2018-07-21 14:28:32 -0500

    removed unused code

--- src/com/egandunning/vba/Main.java
+++ src/com/egandunning/vba/Main.java
@@ -3,8 +3,6 @@ package com.egandunning.vba;
 public class Main {
 
 	public static void main(String args[]) {
-		//SwingUtilities.invokeLater(() -> {
-			new MainWindow();//.setVisible(true);
-		//});
+		new MainWindow();
 	}
 }
